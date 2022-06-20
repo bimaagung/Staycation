37ac0d844caeb439fcc9696947b9997440956f1a
Bima Agung Setya Budi <bimaagungsetya@gmail.com>
2022-06-20 11:05:23 +0700

    Create Mostpicked & Star

---
+++ src/parts/Testimony.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Star from "elements/Star";
+
+export default function Testimony() {
+  return (
+    <div>
+      <Star value={3.45} width={35} height={35} spacing={4}></Star>
+    </div>
+  );
+}
