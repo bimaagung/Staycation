148ab823d3e604d9658ce88a25b2689c7e489b14
Bima Agung Setya Budi <bimaagungsetya@gmail.com>
2022-06-20 13:36:17 +0700

    test star & create testimoni

--- src/parts/Testimony.js
+++ src/parts/Testimony.js
@@ -1,10 +1,40 @@
 import React from "react";
 import Star from "elements/Star";
+import TestimonyAccent from "assets/images/testimonial-landingpages-frame.jpg";
 
-export default function Testimony() {
+export default function Testimony(data) {
   return (
-    <div>
-      <Star value={3.45} width={35} height={35} spacing={4}></Star>
-    </div>
+    <section>
+      <div className="container">
+        <div className="row align-item-center">
+          <div className="col-auto" style={{ marginRight: 70 }}>
+            <div
+              className="testimonial-hero"
+              style={{ margin: `30px 0 0 30px` }}
+            >
+              <img
+                src={data.imageUrl}
+                alt="Testimonial"
+                className="position-absolute"
+                style={{ zIndex: 1 }}
+              />
+              <img
+                src={TestimonyAccent}
+                alt="Testimonial frame"
+                className="position-absolute"
+                style={{ margin: `-30px 0 0 -30px`, zIndex: 1 }}
+              />
+            </div>
+          </div>
+          <div className="col-6">
+            <h4 style={{ marginBottom: 40 }}>{data.name}</h4>
+            <Star value={data.rate} width={35} height={35} spacing={4} />
+            <h5 className="h2 font-weight-light line-height-2 my-3">
+              {data.content}
+            </h5>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
